1fcaacaaf6e7792de9ae19177e6809d99e9a6ba3
Thanh Huynh <64248530+htmthanh@users.noreply.github.com>
2023-01-05 15:55:13 +0700

    update link to signup

--- uoaticket/src/Pages/login.jsx
+++ uoaticket/src/Pages/login.jsx
@@ -10,13 +10,15 @@ function Login(){
         <div className="login-box">
             <h1>Đăng nhập</h1>
             <p>Chưa có tài khoản ?
-            <Link to="/signup">Đăng kí ngay</Link>
+            <Link to='/signup'> Đăng ký ngay.</Link>
             </p>
+            
             <form action="Login" method="post">           
                 <input type="text" name="Name" placeholder="Nhập email hoặc tên đăng nhập" />
                 <input type= "text" name="Password" placeholder="Mật khẩu"/>
                 <input type='checkbox' name="savelogin"></input>
                 <label for="savelogin">Giữ đăng nhập
+                <br></br>
                     <a>Quên mật khẩu</a>
                 </label>
                 <input type="submit" name="Login"></input>  
