a5b49a755cc743e35000f0ade358f8b26792c67c
Thanh Huynh <64248530+htmthanh@users.noreply.github.com>
2023-01-06 12:35:46 +0700

    upload news

--- uoaticket/src/App.js
+++ uoaticket/src/App.js
@@ -6,6 +6,8 @@ import Navbar from './Component/nav-bar';
 import { BrowserRouter as Router, Route, Routes} 
 from "react-router-dom";
 import Home from './Pages/home';
+import News from './Pages/news';
+import Career from './Pages/career';
 import Footer from './Component/footer';
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
         <Route exact path="/signup" element={<Signup/>}/>
         {/* <Route path="*" element={<NoPage/>}/> */}
         <Route exact path="/footer" element={<Footer/>}/>
+        <Route exact path="/news" element={<News/>}/>
+        <Route exact path="/career" element={<Career/>}/>
       </Routes> 
     </Router>
   );
