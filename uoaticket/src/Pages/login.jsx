6a3a1a2545f9213563199ccac6fbc44baf8c3a13
Hi_myname_is_jeff <74254098+hi-myname-is-jeff@users.noreply.github.com>
2023-01-10 12:11:06 +0700

    Login_update

--- uoaticket/src/Pages/login.jsx
+++ uoaticket/src/Pages/login.jsx
@@ -1,42 +1,49 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './login.scss'
+import './login.css'
 import logo from "./images/uoalogo.png"
-import ava from "./images/slider3.jpg"
+import ava from "./images/signupImg.jpg"
 import FacebookIcon from '@mui/icons-material/Facebook';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import GoogleIcon from '@mui/icons-material/Google';
 
 function Login(){
-    return(      
+    return(
+    <>
+    
     <div className="sign-in">
-        <div className="login-box">
-            <h1>Đăng nhập</h1>
-            <p className="signup">Chưa có tài khoản ?
-                <Link to="/signu"className="s1">Đăng kí ngay</Link>
-            </p>
-            
-            <form action="Login" method="post">           
-                <input type="text" name="Name" placeholder="Nhập email hoặc tên đăng nhập" />
-                <input type= "text" name="Password" placeholder="Mật khẩu"/>
-                <input type='checkbox' name="savelogin"></input>
-                <label for="savelogin">Giữ đăng nhập
-                <br></br>
+            <div className="login-box">
+                <h1>Đăng nhập</h1>
+                <p >Chưa có tài khoản? 
+                    <Link to="/signup" className="s1"> Đăng kí ngay</Link>
+                </p>
+
+                <form action="Login" method="post">
+                    <input type="text" name="Name" placeholder="Nhập email hoặc tên đăng nhập" />
+                    <input type="text" name="Password" placeholder="Mật khẩu" />
+                    <input type='checkbox' name="savelogin"></input>
+                    <label for="savelogin"> Giữ đăng nhập
+                        <br></br>
+                        
+                    </label>
+                    <br></br>
                     <a>Quên mật khẩu</a>
-                </label>
-                <input type="submit" name="Login"></input>  
-                <p>Đăng nhập bằng phương thức khác</p>
-            </form>
-            <div className="icon">
-                 <FacebookIcon/>
-                 <GitHubIcon/>
-                 <GoogleIcon/>
+                    <input type="submit" name="Login"></input>
+                    
+                </form>
+                <p>Đăng nhập bằng phương thức khác  </p>
+                
+                <div className="icon">
+                    <FacebookIcon />
+                    <GitHubIcon />
+                    <GoogleIcon />
+                </div>
+            </div>
+            <div className="poster">
+                <img src={ava} alt="poster"></img>
             </div>
         </div>
-        <div className="poster">
-            <img src={ava} alt="poster"></img>
-        </div>
-    </div>);
+        </>);
 }
 
 export default Login;
