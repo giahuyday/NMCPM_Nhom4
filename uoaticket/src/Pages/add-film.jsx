f1042841075f88df835267aeeda8d9e1f40dd77a
tgthinh7 <giathinhtruong0703@outlook.com>
2023-01-10 18:23:29 +0700

    image-handle

--- uoaticket/src/Pages/add-film.jsx
+++ uoaticket/src/Pages/add-film.jsx
@@ -1,21 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import CloseIcon from '@mui/icons-material/Close';
 import "./add-film.css";
 
 const AdminAddFilm = () => {
+
+  const [imageUrl, setImageUrl] = useState("");
+
+  const handleChange = (e) => {
+    const file = e.target.files[0];
+    const objectUrl = URL.createObjectURL(file);
+    setImageUrl(objectUrl);
+  }
+
+  const handleRemove = () => {
+    setImageUrl("");
+    document.querySelector(".uploadpic").value = "";
+  }
+
   return (
     <div className="admin-add-film">
       <div className="admin-add-film-child" />
       <button className="exit">
         <CloseIcon/>
       </button>
-      <button className="cancel">Nhập lại
-      </button>
+
+      {imageUrl && <button className="cancel" onClick={handleRemove}>Nhập lại
+      </button>}
+      
       <button className="summit">Cập nhật
       </button>
       <input className="namefilm" type="text" placeholder="Tên film" required />
       <input className="detail" type="text" placeholder="Mô tả" required />
-      <input className="uploadpic" type="file" required />
+      <input className="uploadpic" type="file" required onChange={handleChange} />
+      { imageUrl && <img className="preview-image" src={imageUrl} alt="uploaded image" /> }
     </div>
   );
 };
